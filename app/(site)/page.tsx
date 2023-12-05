8b2a124e9037dc335e64d4132868fa6f21893544
Rhys Clark <rhysclark14@yahoo.com>
2023-12-05 13:48:18 +0000

    Created Basic Layout of Login Page

---
+++ app/(site)/page.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from 'next'
+import AuthForm from '../_components/AuthForm'
+
+export const metadata: Metadata = {
+  title: 'Messenger Clone',
+  description: '',
+}
+
+export default function Home() {
+  return (
+    <div
+    className="
+    flex
+    min-h-full
+    flex-col
+    justify-center
+    py-15
+    sm:px-6
+    lg:px-8
+    bg-gray-100
+    "
+    >
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h1 
+        className="
+          text-center
+          text-3xl
+          font-bold
+          tracking-tight
+          text-gray-900
+        "
+        >
+          Login or Continue as Guest.
+        </h1>
+      </div>
+      <AuthForm/>
+    </div>
+  )
+}
